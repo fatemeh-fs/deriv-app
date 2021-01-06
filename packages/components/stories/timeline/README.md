4597b94f9eaa641d5c460c501d17fe6283751e3b
Fatemeh <fatemeh@firstsource.tech>
2021-01-06 18:38:29 +0330

    timeline component note added to storybook

---
+++ packages/components/stories/timeline/README.md
@@ -0,0 +1,49 @@
+# Timeline Component
+
+A timeline is a graphical representation of a chronological sequence of events.
+
+## Usage
+
+```jsx
+import Toast from 'deriv-components';
+
+const DummyComponent = props => (
+    <Timeline>
+        <Item item_title='Title'>
+            <div>test</div>
+        </Item>
+        <Item item_title='Title2'>
+            <div>test2</div>
+        </Item>
+        <Item item_title='Title3'>
+            <div>test3</div>
+        </Item>
+    </Timeline>
+);
+```
+
+## Props
+
+| Name       | Type     | Default | Description                     |
+| ---------- | -------- | ------- | ------------------------------- |
+| item_title | {string} | null    | It is the prop of Timeline.Item |
+
+# Full example:
+
+```jsx
+import Toast from 'deriv-components';
+
+const DummyComponent = props => (
+    <Timeline>
+        <Timeline.Item item_title='Title'>
+            <div>test</div>
+        </Timeline.Item>
+        <Timeline.Item item_title='Title2'>
+            <div>test2</div>
+        </Timeline.Item>
+        <Timeline.Item item_title='Title3'>
+            <div>test3</div>
+        </Timeline.Item>
+    </Timeline>
+);
+```
--- packages/components/stories/toast/toast.stories.js
+++ packages/components/stories/toast/toast.stories.js
@@ -3,7 +3,6 @@ import { boolean, number, withKnobs } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
 import React from 'react';
 import Toast from 'Components/toast';
-import Theme from '../shared/theme';
 import notes from './README.md';
 
 const stories = storiesOf('Toast', module);
@@ -11,27 +10,17 @@ const stories = storiesOf('Toast', module);
 stories.addDecorator(withKnobs).addDecorator(withInfo);
 
 stories
-    .add(
-        'static',
-        () => (
-            <Theme is_dark={boolean('Theme', false)}>
-                <Toast>Message to be shown!</Toast>
-            </Theme>
-        ),
-        { notes }
-    )
+    .add('static', () => <Toast>Message to be shown!</Toast>, { notes })
     .add(
         'with timeout',
         () => (
-            <Theme is_dark={boolean('Theme', false)}>
-                <Toast
-                    is_open={boolean('is open?', true)}
-                    timeout={number('Timeout', 3000)}
-                    onClose={console.log('Closed')}
-                >
-                    Message
-                </Toast>
-            </Theme>
+            <Toast
+                is_open={boolean('is open?', true)}
+                timeout={number('Timeout', 3000)}
+                onClose={console.log('Closed')}
+            >
+                Message
+            </Toast>
         ),
         { notes }
     );
